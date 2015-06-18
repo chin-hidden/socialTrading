c69a36efb68026cd7a42bc81753510b8cdad9a91
Trung Ngo <ndtrung4419@gmail.com>
2015-06-18 11:22:26 +0700

    Clean up Application

--- src/main/java/vn/com/vndirect/socialtrading/Application.java
+++ src/main/java/vn/com/vndirect/socialtrading/Application.java
@@ -1,33 +1,14 @@
 package vn.com.vndirect.socialtrading;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.datasource.AbstractDataSource;
-import vn.com.vndirect.socialtrading.dao.FollowerDao;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-
-class MyDataSource extends AbstractDataSource {
-    @Override
-    public Connection getConnection() throws SQLException {
-        return getConnection("postgres", "1111");
-    }
-
-    @Override
-    public Connection getConnection(String username, String password) throws SQLException {
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/duber",
-                username, password);
-    }
-}
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        // SpringApplication.run(Application.class, args);
-        FollowerDao followerDao = new FollowerDao(new MyDataSource());
-        System.out.println(followerDao.findAll().size());
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 }
