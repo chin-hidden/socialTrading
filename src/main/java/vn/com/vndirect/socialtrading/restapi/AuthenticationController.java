df0c835206f370831111f5638e3f7e888bf4cb2a
Trung Ngo <ndtrung4419@gmail.com>
2015-06-22 15:03:56 +0700

    Refactor the authentication route

--- src/main/java/vn/com/vndirect/socialtrading/restapi/AuthenticationController.java
+++ src/main/java/vn/com/vndirect/socialtrading/restapi/AuthenticationController.java
@@ -2,7 +2,12 @@ package vn.com.vndirect.socialtrading.restapi;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import vn.com.vndirect.socialtrading.dao.AccountDao;
 import vn.com.vndirect.socialtrading.dao.FollowerDao;
@@ -14,14 +19,36 @@ import java.util.Optional;
 
 @RestController
 public class AuthenticationController {
-    @Autowired private HttpSession session;
-    @Autowired private AuthenticationService as;
-    @Autowired private FollowerDao followerDao;
-    @Autowired private AccountDao accountDao;
-    //@Autowired private TraderDao traderDao;
+    private HttpSession session;
+    private AuthenticationService as;
+    private FollowerDao followerDao;
+    private AccountDao accountDao;
 
-    @RequestMapping(value = "/api/v1/login")
-    public String login(String username, String password) {
+    @Autowired
+    public AuthenticationController(HttpSession session,
+                                    AuthenticationService as,
+                                    FollowerDao followerDao,
+                                    AccountDao accountDao) {
+        this.session = session;
+        this.as = as;
+        this.followerDao = followerDao;
+        this.accountDao = accountDao;
+    }
+
+    @RequestMapping(value = "/api/v1/login", method = RequestMethod.GET)
+    public ResponseEntity<Account> loginInfo() {
+
+        Account account = (Account) session.getAttribute("user");
+        if (account == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } else {
+            return new ResponseEntity<>(account, HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "/api/v1/login", method = RequestMethod.POST)
+    public String login(@RequestParam String username,
+                        @RequestParam String password) {
         boolean authenticated = as.authenticate(username, password);
 
         if (authenticated) {
