0374bbaf5cebca95bf88eaafb12d9b703a483cdb
Trung Ngo <ndtrung4419@gmail.com>
2015-06-25 15:34:31 +0700

    Add basic security measures with spring-security
    - Two hardcoded users: user1, user2, same password as "password"
    - Every request must be authenticated

---
+++ src/main/java/vn/com/vndirect/socialtrading/SecurityConfig.java
@@ -0,0 +1,28 @@
+package vn.com.vndirect.socialtrading;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("password").roles("USER").and()
+                .withUser("user2").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
+
