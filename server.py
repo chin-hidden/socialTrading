2f0fe7a96ddc38c8f0659a9ca62653bc5c266cf5
Trung Ngo <ndtrung4419@gmail.com>
2015-07-16 14:13:59 +0700

    Fixture data for some API routes
    - /account
    - /followers
    - /follower/<username>
    - /follower/<username>/following
    - /follower/<username>/positions
    - /traders

--- server.py
+++ server.py
@@ -65,12 +65,28 @@ class User:
         return False
 
     def get_id(self):
-        return "trung"
+        return self.id
+
+
+class UserDao:
+    def get_user_by_username(self, username):
+        if username == "ndtrung4419":
+            user = User()
+            user.id = username
+            user.name = "Trung Ngo"
+            user.type = "FOLLOWER"
+            user.accountNumber = "1234"
+            user.broker = "VND"
+            user.riskFactor = 60
+            user.firstLogin = False
+            return user
+
 
 
 @login_manager.user_loader
 def load_user(userid):
-    return User()
+    dao = UserDao()
+    return dao.get_user_by_username(userid)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -88,7 +104,10 @@ def login():
         </html>
         """
     else:
-        login_user(User())
+        # FIXME Use Flask-WTF to validate the form format
+        user_dao = UserDao()
+        user = user_dao.get_user_by_username(request.form["username"])
+        login_user(user)
 
         client = VndirectTradeApiClient()
         token = client.login(request.form["username"], request.form["password"])
@@ -96,7 +115,8 @@ def login():
         return token
 
 
-@app.route("/logout")
+# FIXME: GET should not have side effect
+@app.route("/logout", methods=["GET", "POST"])
 @login_required
 def logout():
     logout_user()
@@ -104,11 +124,102 @@ def logout():
 
 
 @app.route("/")
+def index():
+    return redirect("/static/index.html")
+
+
+@app.route("/api/v1/account")
 @login_required
-def hello():
-    client = VndirectTradeApiClient()
-    token = session["trade-api-token"]
-    return jsonify(**client.get_user_detail(token))
+def account_detail():
+    """\
+    Get the current user's detail
+    """
+    user_dao = UserDao()
+    user = user_dao.get_user_by_username(session["user_id"])
+    return jsonify(result=user.__dict__)
+
+
+@app.route("/api/v1/followers", methods=["GET"])
+def get_all_followers():
+    """\
+    Return all followers
+    """
+    return jsonify(result=[])
+
+
+@app.route("/api/v1/follower/<username>", methods=["GET", "PUT"])
+def follower(username):
+    """\
+    GET and PUT a follower.
+
+    GET /follower/<username>
+    {
+        "name": "Superman",
+        "accountNumber": "1234",
+        "type": "FOLLOWER",    // or TRADER
+        "riskFactor": 60
+    }
+    """
+    user_dao = UserDao()
+    user = user_dao.get_user_by_username(username)
+
+    if request.method == "PUT":
+        print(request.form)
+
+    return jsonify(result=user.__dict__)
+
+
+@app.route("/api/v1/follower/<username>/following", methods=["GET", "POST"])
+def following_relationships(username):
+    if request.method == "POST":
+        pass
+
+    return jsonify(result=[])
+
+
+@app.route("/api/v1/follower/<username>/positions")
+def follower_positions(username):
+    return jsonify(result=[])
+
+
+@app.route("/api/v1/traders")
+def get_all_traders():
+    """\
+    Return all traders
+    """
+    traders = [
+    {
+        "name": "Nguyen Hoang Giang",
+        "username": "giangnguyen",
+        "type": "TRADER",
+        "accountNumber": "1234",
+        "description": "lol",
+        "cash": 1234,
+        "peopleFollowing": 10,
+        "roi": 100
+    },
+    {
+        "name": "Tran Vu Thach",
+        "username": "thachvu",
+        "type": "TRADER",
+        "accountNumber": "1234",
+        "description": "lol",
+        "cash": 1234,
+        "peopleFollowing": 10,
+        "roi": 100
+    },
+    {
+        "name": "Hoang Minh Chau",
+        "username": "chauhoang",
+        "type": "TRADER",
+        "accountNumber": "1234",
+        "description": "lol",
+        "cash": 1234,
+        "peopleFollowing": 10,
+        "roi": 100
+    },
+    ]
+    return jsonify(result=traders)
 
 
 if __name__ == "__main__":
