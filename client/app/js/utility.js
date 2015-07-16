be1f1a3dd202f5efb47ba9b3e5da4f8c12fe60d5
Trung Ngo <ndtrung4419@gmail.com>
2015-07-16 16:38:49 +0700

    Remove 'VN' in formatVNCurrency, add more formatting util methods

--- client/app/js/utility.js
+++ client/app/js/utility.js
@@ -1,6 +1,14 @@
-function formatVNCurrency(amount) {
+function formatCurrency(amount) {
     return amount.toLocaleString("vi", {
         style: "currency",
         currency: "VND",
     });
+}
+
+function formatAmount(amount) {
+	return amount.toLocaleString("vi");
+}
+
+function formatPercent(amount) {
+	return amount.toLocaleString("vi", {style: "percent"});
 }
