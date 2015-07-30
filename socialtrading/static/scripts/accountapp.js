c4664157c90ca27d83d1e622290836a0eb5d9b41
Trung Ngo <ndtrung4419@gmail.com>
2015-07-30 18:07:24 +0700

    Remove the compiled socialtrading/static/ folder

--- socialtrading/static/scripts/accountapp.js
+++
@@ -1,8 +0,0 @@
-define(["exports", "common", "csrf", "accountScreen"], function (exports, _common, _csrf, _accountScreen) {
-    "use strict";
-
-    (0, _common.loadData)().then(function () {
-        React.render(React.createElement(_accountScreen.AccountScreen, null), document.getElementById('app'));
-    });
-});
-//# sourceMappingURL=accountapp.js.map
