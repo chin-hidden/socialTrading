7afeba7d9174fa6328fdea8e88b3b849c8f42f4f
Trung Ngo <ndtrung4419@gmail.com>
2015-08-11 15:10:20 +0700

    Add Vagrantfile and its bootstrap script

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "chef/centos-7.0"
+  # config.vm.provider "virtualbox" do |v|
+  #   v.gui = true
+  # end
+
+  # config.vm.network "private_network", type: "dhcp"
+  # config.vm.network "public_network"
+  # config.vm.provision :shell, path: "bootstrap.sh"
+
+
+  # Enable provisioning with a shell script. Additional provisioners such as
+  # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
+  # documentation for more information about their specific syntax and use.
+  # config.vm.provision "shell", inline: <<-SHELL
+  #   sudo apt-get update
+  #   sudo apt-get install -y apache2
+  # SHELL
+end
---
+++ bootstrap.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -o xtrace
+
+YUM="yum -y"
+rpm --import http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-7
+# $YUM update --exclude="kernel*"
+
+$YUM groupinstall 'Development Tools'
+
+# NodeJS
+curl -sL https://rpm.nodesource.com/setup | bash -
+$YUM install nodejs
+
+
+# Installing RabbitMQ
+wget --continue http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+$YUM install epel-release-7*.rpm
+sed --in-place=bak -s 's|#baseurl.*|baseurl=http://mirrors.vinahost.vn/epel/7/$basearch|' /etc/yum.repos.d/epel.repo
+sed --in-place=bak -s 's/mirrorlist/#mirrorlist/' /etc/yum.repos.d/epel.repo
+
+wget --continue http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.2/rabbitmq-server-3.2.2-1.noarch.rpm
+rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+$YUM install erlang
+$YUM install rabbitmq-server-3.2.2-1.noarch.rpm
+
+# Enable the management plugin
+rabbitmq-plugins enable rabbitmq_management
+
+# Make it start on startup
+chkconfig rabbitmq-server on
+/sbin/service rabbitmq-server start
+
+
+$YUM install postgresql-server postgresql-contrib
+systemctl enable postgresql.service
+systemctl start postgresql.service
+
+sudo -u postgres createuser --superuser vagrant
+dropdb duber
+createdb duber
+
+
+# Misc tools
+$YUM install vim man byobu bash-completion
