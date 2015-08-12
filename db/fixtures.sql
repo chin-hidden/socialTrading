2bb2ff8ba1fe951189af2ccce4a86a506d8c32fc
Trung Ngo <ndtrung4419@gmail.com>
2015-08-12 13:55:14 +0700

    Replace the traders in the fixtures with 10 top accounts

--- db/fixtures.sql
+++ db/fixtures.sql
@@ -5,30 +5,35 @@
 
 
 INSERT INTO account (account_number, username, password, name, cash, account_type, broker) VALUES
-       ('0001210254', 'thachvu', 'password', 'Trần Vũ Thạch', 100000000, 'FOLLOWER', 'VNDIRECT'),
        ('0001032425', 'thangnt.nhtck47', 'password', 'Nguyễn Toàn Thắng', 100000000, 'FOLLOWER', 'VNDIRECT'),
-       ('0001052458', 'chauhoang', 'password', 'Hoàng Minh Châu', 56560000, 'TRADER', 'VNDIRECT'),
-       ('0001210287', 'linhmai', 'password', 'Bùi Mai Linh', 15000000, 'FOLLOWER', 'VNDIRECT'),
-       ('0001041716', 'giangnguyen', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
-       ('0001011079', 'ndtrung4419', 'password', 'Ngô Đức Trung', 45440000, 'TRADER', 'VNDIRECT');
+       ('0101807860', 'trader01', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001008969', 'trader02', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0101018086', 'trader03', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001068688', 'trader04', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('3101000330', 'trader05', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001888989', 'trader06', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001569999', 'trader07', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001111987', 'trader08', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0304008739', 'trader09', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001388888', 'trader10', 'password', 'Nguyễn Hoàng Giang', 4500000, 'TRADER', 'VNDIRECT');
 
 
 INSERT INTO followerInfo (username, risk_factor) VALUES
-       ('thachvu', 60),
-       ('thangnt.nhtck47', 60),
-       ('linhmai', 40);
+       ('thangnt.nhtck47', 60);
 
 INSERT INTO traderInfo (username, description) VALUES
-	('giangnguyen', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
-	('chauhoang', 'Etiam vulputate tristique libero. Vivamus eu lectus non orci rhoncus commodo.'),
-	('ndtrung4419', 'Vivamus vestibulum dolor et consequat semper. Nunc ac elit volutpat, ullamcorper magna eu, pharetra diam.');
+       ('trader01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader02', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader03', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader04', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader05', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader06', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader07', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader08', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+       ('trader09', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.'),
+	('trader10', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sapien a nisl mollis iaculis.');
 
 
 -- for now, we only allow single following relationship
 INSERT INTO following (follower, trader, allocated_money) VALUES
-	('thachvu', 'ndtrung4419', 100000000),
-	('linhmai', 'giangnguyen', 20000000);
-
-
-INSERT INTO position (username, mimicking_username, symbol, quantity, buying_price, buying_date) VALUES
-	('thachvu', 'ndtrung4419', 'VND', 1000, 15600, now());
+	('thangnt.nhtck47', 'trader01', 100000000);
