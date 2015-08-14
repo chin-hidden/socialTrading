84c47f252b17452f90b47cd1ae25ba1c7bfff572
Trung Ngo <ndtrung4419@gmail.com>
2015-08-14 15:22:31 +0700

    Tweak the db fixtures

--- db/fixtures.sql
+++ db/fixtures.sql
@@ -7,7 +7,7 @@
 INSERT INTO account (account_number, username, password, name, cash, account_type, broker) VALUES
        ('0001032425', 'thangnt.nhtck47', 'password', 'Nguyễn Toàn Thắng', 100000000, 'FOLLOWER', 'VNDIRECT'),
        ('0101807860', 'trader01', 'password', 'Thuyền Trưởng Huck', 4500000, 'TRADER', 'VNDIRECT'),
-       ('0001008969', 'trader02', 'password', 'Thanh Chương Tiên Sinh', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001008969', 'trader02', 'password', 'Thanh Tiên Sinh', 4500000, 'TRADER', 'VNDIRECT'),
        ('0101018086', 'trader03', 'password', 'Bàn Tay Vàng', 4500000, 'TRADER', 'VNDIRECT'),
        ('0001068688', 'trader04', 'password', 'Hiệp Sĩ Bóng Đêm', 4500000, 'TRADER', 'VNDIRECT'),
        ('3101000330', 'trader05', 'password', 'Cao Thủ Lướt Sóng', 4500000, 'TRADER', 'VNDIRECT'),
