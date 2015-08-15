f3a8bb5b7f7ddfe3b9b2bba37596653fe76d8f0c
Trung Ngo <ndtrung4419@gmail.com>
2015-08-15 13:41:06 +0700

    Refactor tradeapi.py to return data in a more static typed approach

--- socialtrading/tradeapi.py
+++ socialtrading/tradeapi.py
@@ -1,63 +1,24 @@
+import typing
 import requests
 from enum import Enum
 
 
-class FakeVndirectTradeApiClient:
-    def login(self, username, password):
-        return "thisisatoken"
-
-    def get_user_detail(self):
-        return {
-          "customerId": "0001728170",
-          "customerName": "ABC",
-          "accounts": [
-            {
-              "accountNumber": "0001019651",
-              "fullName": "ABC"
-            },
-            {
-              "accountNumber": "0001032423",
-              "fullName": "ABC, jr."
-            }
-          ]
-        }
-
-    def get_account_detail(self, account_nr):
-        return {
-            u'accountNumber': account_nr,
-            u'purchasePower': 3453,
-            u'cash': 34324,
-            u'NAV': 64545,
-            u'withdrawable': 55656,
-            'portfolio': {
-                u'totalCost': 34324,
-                u'ratio': 32432,
-                u'accountNumber': account_nr,
-                u'profit': 7564,
-                u'totalCurrentValue': 3243252,
-                u'stocks': [
-
-                ],
-                u'customerId': u'0001728170'
-            }
-        }
-
 
 class OrderType(Enum):
-    ATO = ("ATO", '1', '2')
-    LO  = ("LO",  '2', '0')
-    ATC = ("ATC", '1', '7')
-    MP  = ("MP",  '1', '0')
-    MOK = ("MOK", '1', '4')
-    MAK = ("MAK", '1', '3')
-    MTL = ("MTL", '1', '0')
+    ATO = ("ATO", 1, 2)
+    LO  = ("LO",  2, 0)
+    ATC = ("ATC", 1, 7)
+    MP  = ("MP",  1, 0)
+    MOK = ("MOK", 1, 4)
+    MAK = ("MAK", 1, 3)
+    MTL = ("MTL", 1, 0)
 
     @property
-    def numeric_order_type(self):
+    def numeric_order_type(self) -> int:
         return self.value[1]
 
     @property
-    def time_in_force(self):
+    def time_in_force(self) -> int:
         return self.value[2]
 
 
@@ -83,7 +44,7 @@ class VndirectOrsClient:
                     order_type: OrderType,
                     symbol: str,
                     price: float,
-                    quantity: int):
+                    quantity: int) -> dict:
         """\
         Place an order using VNDirect's ORS subsystem.
         """
@@ -116,6 +77,117 @@ class VndirectOrsClient:
             return '2'
 
 
+class JsonWrapper:
+    @classmethod
+    def from_json(cls, json_blob: dict):
+        obj = cls()
+        obj._json = json_blob
+        return obj
+
+
+class Account(JsonWrapper):
+    @property
+    def account_number(self) -> str:
+        return self._json['accountNumber']
+
+    @property
+    def full_name(self) -> str:
+        return self._json['fullName']
+
+
+class UserDetail(JsonWrapper):
+    @property
+    def customer_id(self) -> str:
+        return self._json['customerId']
+
+    @property
+    def customer_name(self) -> str:
+        return self._json['customerName']
+
+    @property
+    def accounts(self) -> typing.List[Account]:
+        return [Account.from_json(account_json) for account_json in self._json['accounts']]
+
+
+class Stock(JsonWrapper):
+    @property
+    def cost(self) -> float:
+        return self._json['cost']
+
+    @property
+    def cost_price(self) -> float:
+        return self._json['costPrice']
+
+    @property
+    def current_price(self) -> float:
+        return self._json['currentPrice']
+
+    @property
+    def profit(self) -> float:
+        return self._json['gainLoss']
+
+    @property
+    def roi(self) -> float:
+        return self._json['gainLossRatio']
+
+    @property
+    def quantity(self) -> int:
+        return self._json['quantity']
+
+    @property
+    def stock_symbol(self) -> str:
+        return self._json['symbol']
+
+
+class Portfolio(JsonWrapper):
+    @property
+    def total_cost(self) -> float:
+        return self._json['totalCost']
+
+    @property
+    def ratio(self) -> float:
+        return self._json['ratio']
+
+    @property
+    def profit(self) -> float:
+        return self._json['profit']
+
+    @property
+    def total_current_value(self) -> float:
+        return self._json['totalCurrentValue']
+
+    @property
+    def stocks(self) -> typing.List[Stock]:
+        return [Stock.from_json(stock_json) for stock_json in self._json['stocks']]
+
+
+class AccountDetail(JsonWrapper):
+    @property
+    def account_number(self) -> str:
+        return self._json['accountNumber']
+
+    @property
+    def cash(self) -> float:
+        return self._json['cash']
+
+    @property
+    def purchase_power(self) -> float:
+        return self._json['purchasePower']
+
+    @property
+    def nav(self) -> float:
+        return self._json['NAV']
+
+    @property
+    def withdrawable(self) -> float:
+        return self._json['withdrawable']
+
+    @property
+    def portfolio(self) -> Portfolio:
+        return Portfolio.from_json(self._json['portfolio'])
+
+
+
 class VndirectTradeApiClient:
     """\
     Client to communicate with VNDIRECT's TradeAPI.  Offial documentation at:
@@ -128,7 +200,7 @@ class VndirectTradeApiClient:
     AUTH_URL = "https://api.vndirect.com.vn/auth"
     API_URL = "https://api.vndirect.com.vn/trade"
 
-    def login(self, username, password):
+    def login(self, username: str, password: str) -> str:
         """\
         Log the user in.
 
@@ -142,7 +214,7 @@ class VndirectTradeApiClient:
         self.token = res.json()["token"]
         return self.token
 
-    def get_vtos(self):
+    def get_vtos(self) -> typing.List[str]:
         """
         Get the VTOS challenges.
 
@@ -153,7 +225,7 @@ class VndirectTradeApiClient:
         return requests.get(self.AUTH_URL + "/vtos",
                             headers=headers).json()['challenges']
 
-    def login_vtos(self, v1, v2, v3):
+    def login_vtos(self, v1: str, v2: str, v3: str):
         """
         Second phase login.
 
@@ -172,35 +244,16 @@ class VndirectTradeApiClient:
         self.token = res.json()['token']
         return self.token
 
-    def get_user_detail(self):
+    def get_user_detail(self) -> UserDetail:
         """\
         Get the basic details of the authenticated user.
-
-        Returns:
-            dict: of the following format
-
-            {
-              "customerId": "0001728170",
-              "customerName": "ABC",
-              "accounts": [
-                {
-                  "accountNumber": "0001019651",
-                  "fullName": "ABC"
-                },
-                {
-                  "accountNumber": "0001032423",
-                  "fullName": "ABC, jr."
-                }
-              ]
-            }
         """
         headers = {"X-AUTH-TOKEN": self.token}
 
         res = requests.get(self.API_URL + "/customer", headers=headers)
-        user = res.json()
-        return user
+        return UserDetail.from_json(res.json())
 
-    def get_account_detail(self, account_nr):
+    def get_account_detail(self, account_nr: str) -> AccountDetail:
         """\
         Get the details of an account. It is the consolidation of the following API endpoints:
 
@@ -223,4 +276,4 @@ class VndirectTradeApiClient:
         res = requests.get(url, headers=headers)
         account['portfolio'] = res.json()
 
-        return account
+        return AccountDetail.from_json(account)
