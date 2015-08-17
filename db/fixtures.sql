cba5c559518dd6aa5dd2a51a52106232de0c7e3d
Trung Ngo <ndtrung4419@gmail.com>
2015-08-17 16:26:33 +0700

    Change some fake trader names in the fixture

--- db/fixtures.sql
+++ db/fixtures.sql
@@ -7,8 +7,8 @@
 INSERT INTO account (account_number, username, password, name, cash, account_type, broker) VALUES
        ('0001234567', 'demo.user', 'password', 'Người sử dụng Demo', 100000000, 'FOLLOWER', 'VNDIRECT'),
        ('0001032425', 'thangnt.nhtck47', 'password', 'Nguyễn Toàn Thắng', 100000000, 'FOLLOWER', 'VNDIRECT'),
-       ('0101807860', 'trader01', 'password', 'Thuyền Trưởng Huck', 4500000, 'TRADER', 'VNDIRECT'),
-       ('0001008969', 'trader02', 'password', 'Thanh Tiên Sinh', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0101807860', 'trader01', 'password', 'Xin Một Lần Thua', 4500000, 'TRADER', 'VNDIRECT'),
+       ('0001008969', 'trader02', 'password', 'Lỗ Là Gì', 4500000, 'TRADER', 'VNDIRECT'),
        ('0101018086', 'trader03', 'password', 'Bàn Tay Vàng', 4500000, 'TRADER', 'VNDIRECT'),
        ('0001068688', 'trader04', 'password', 'Hiệp Sĩ Bóng Đêm', 4500000, 'TRADER', 'VNDIRECT'),
        ('3101000330', 'trader05', 'password', 'Cao Thủ Lướt Sóng', 4500000, 'TRADER', 'VNDIRECT'),
