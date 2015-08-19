b3e507a9c0389c1e067569e2b81e466b458c9b9b
Trung Ngo <ndtrung4419@gmail.com>
2015-08-19 16:22:11 +0700

    Refactor the cloner code

--- socialtrading/cloner.py
+++ socialtrading/cloner.py
@@ -6,10 +6,10 @@ Code pertaining to the order cloning process.
 For more details about data types, refer to: https://ivnd.vndirect.com.vn/display/RD/NotiCenter
 """
 
+import copy
 import kombu
 import kombu.mixins
 import logging
-import time
 import datetime
 import typing
 
@@ -36,15 +36,17 @@ ExecutedOrderResponse = typing.NamedTuple('ExecutedOrderResponse',
 
 
 def parse_executed_order_response(blob: dict) -> ExecutedOrderResponse:
+    blob = copy.copy(blob)
     blob['transactTime'] = datetime.datetime.fromtimestamp(blob['transactTime'] / 1000)
     blob['side'] = OrderSide.from_int(blob['side'])
     return ExecutedOrderResponse(**blob)
 
 
 Order = typing.NamedTuple('Order',
-    [('account_number', str),
+    [('username', str),
+     ('account_number', str),
      ('price', float),
-     ('stock_symbol', str),
+     ('symbol', str),
      ('quantity', int),
      ('side', OrderSide),
      ('type', OrderType)])
@@ -57,6 +59,73 @@ class OrderProcessor:
         self.message_router = message_router
         self.user_service = user_service
 
+    def send_follower_order(self,
+                            order: Order,
+                            sender: models.Follower,
+                            micmicking: models.Trader):
+        trans = models.Transaction()
+
+        # Send the order to the broker
+        if sender.broker == "VND":
+            # FIXME: Connect with the ORS client here.
+            pass
+        elif sender.broker == "__DEMO__":
+            trans.order_id = datetime.datetime.now().__str__()
+
+        trans.username = sender.username
+        trans.mimicking_username = micmicking.username
+        trans.symbol = order.symbol
+        trans.price = order.price
+        trans.quantity = order.quantity
+        trans.side = order.side.value
+        trans.type = order.type.value
+        trans.date = datetime.datetime.now()
+
+        if order.side == models.OrderSide.NORMAL_BUY:
+            sender.cash -= order.price * order.quantity
+
+            deal = models.Deal()
+            deal.buying_transaction = trans
+
+            models.db.session.add(deal)
+            models.db.session.add(sender)
+
+            self.message_router.send_message_to_user(
+                sender.username,
+                "deal:created",
+                {"deal_id": deal.id})
+
+            self.message_router.send_message_to_user(
+                sender.username,
+                "user:updated", {})
+        else:
+            # FIXME: Lots of processing here!
+            self.message_router.send_message_to_user(
+                sender.username,
+                "deal:updated",
+                {"deal_id": deal.id})
+
+        models.db.session.add(trans)
+        models.db.session.commit()
+
+        if sender.is_demo_account:
+            pass
+            # Pretend that the order has been matched completely immediately.
+            # demo_account_res = {
+            #     'orderId': trans.order_id,
+            #     'account': sender.account,
+            #     'transactTime', datetim: ,
+            #     'tradeDate': ,
+            #     'side': ,
+            #     'symbol': ,
+            #     'qty': ,
+            #     'price': ,
+            #     'matchedQty': ,
+            #     'matchedPrice': ,
+            # }
+            # demo_account_res['matchedQty'] = demo_account_res['qty']
+            # self.process_follower_message(sender, demo_account_res)
+
     def process_trader_message(self, trader, message):
         trader_order = parse_executed_order_response(message)
         cloned_orders = self.clone_trader_order(trader, trader_order)
@@ -64,31 +133,21 @@ class OrderProcessor:
         for order in cloned_orders:
             # Find the corresponding session and place the order
             # using that session's trade api client.
-            follower = self.user_service \
-                .get_user_by_account_number(order.account_number, broker="VNDIRECT")
+            follower = self.user_service.get_user_by_username(order.username)
 
-            if follower.is_demo_account:
-                # Pretend that the order has been matched completely immediately.
-                self.process_follower_message(follower, {
-                    "lol": "lol"
-                })
-
-            # FIXME: Blocking operation. Should leverage some kind of
-            # async capability.
-            time.sleep(0.5)
-
-            # After placing the order, open a transaction with that
-            # order for further order notifications.
+            self.send_follower_order(order, follower, trader)
 
     def process_follower_message(self, follower, message):
         # FIXME: Is this an executed notification for an order we placed?
+        res = parse_executed_order_response(message)
 
+        deal = models.deal_service.get_deal_by_order_id(res.orderId)
 
         # Notification
         self.message_router.send_message_to_user(
             follower.username,
-            "order_executed",
-            message)
+            "deal:updated",
+            {"deal_id": deal.id})
 
     def clone_trader_order(self,
                            trader: models.Trader,
@@ -112,9 +171,10 @@ class OrderProcessor:
 
             new_price = order.price
             cloned = Order(
+                username=follower.username,
                 account_number=follower.account_number,
                 price=new_price,
-                stock_symbol=order.symbol,
+                symbol=order.symbol,
                 quantity=follower.next_money_slot() // new_price,
                 side=order.side,
                 type=OrderType.MP
